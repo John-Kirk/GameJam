fccd0b6edad9002379f611353d1bc4b420fd6168
John Kirk <john.kirk.dev@gmail.com>
2020-11-07 11:49:09 +0000

    Rendering planets via an actor and stage

--- core/src/com/nerds/gamejam/screen/GameScreen.java
+++ core/src/com/nerds/gamejam/screen/GameScreen.java
@@ -1,30 +1,54 @@
 package com.nerds.gamejam.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.nerds.gamejam.GameJam;
+import com.nerds.gamejam.actor.PlanetActor;
+import com.nerds.gamejam.gameplay.planet.PlanetFactory;
 
 public class GameScreen extends ScreenAdapter {
 
     private final GameJam game;
     private final Stage stage;
     private final Skin skin;
+    private PlanetFactory planetFactory;
+    private PlanetActor planetActor;
 
     public GameScreen(GameJam game, Stage stage, Skin skin) {
         this.game = game;
         this.stage = stage;
         this.skin = skin;
+        this.planetFactory = new PlanetFactory();
     }
 
     @Override
     public void show() {
         Table table = new Table();
-        table.add(new Label("Hello there!", skin)).expand();
+        planetActor = new PlanetActor(planetFactory.createPlanet());
+        planetActor.setWidth(128);
+        planetActor.setHeight(128);
+
+        stage.addListener(new InputListener() {
+            @Override
+            public boolean keyUp(InputEvent event, int keycode) {
+                if (keycode == Input.Keys.SPACE) {
+                    planetActor.setPlanet(planetFactory.createPlanet());
+                }
+
+                return false;
+            }
+        });
+
+        table.add(planetActor).expand();
         table.setFillParent(true);
         stage.addActor(table);
     }
