765f3cac9cc78f4070cbb83cb475294e9ea1b890
John Kirk <john.kirk.dev@gmail.com>
2020-11-08 11:42:16 +0000

    Terrible code to show examples of linking planets, plz don't judge me

--- core/src/com/nerds/gamejam/screen/GameScreen.java
+++ core/src/com/nerds/gamejam/screen/GameScreen.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
 import com.nerds.gamejam.GameJam;
 import com.nerds.gamejam.actor.AnimatedImage;
@@ -41,8 +42,39 @@ public class GameScreen extends ScreenAdapter {
         Animation<TextureRegion> textureRegionAnimation = new Animation<>(0.5f, textureRegionArray);
         AnimatedImage animatedImage = new AnimatedImage(textureRegionAnimation);
         animatedImage.setColor(Color.LIME);
-        table.add(animatedImage).fillX().expandX();
-        table.add(new PlanetActor(planetFactory.createPlanet())).pad(10);
+        AnimatedImage animatedImage2 = new AnimatedImage(textureRegionAnimation);
+        animatedImage2.setColor(Color.LIME);
+        animatedImage2.rotateBy(20);
+        animatedImage2.setOrigin(0, animatedImage2.getHeight() / 2);
+        AnimatedImage animatedImage3 = new AnimatedImage(textureRegionAnimation);
+        animatedImage3.setColor(Color.LIME);
+        animatedImage3.rotateBy(-20);
+        animatedImage3.setOrigin(0, animatedImage3.getHeight() / 2);
+
+        Image image = new Image(new Texture("stripe/stripe_5.png"));
+        image.rotateBy(20);
+        image.setOrigin(0, image.getHeight() / 2);
+        image.setColor(Color.LIME);
+        Image image2 = new Image(new Texture("stripe/stripe_5.png"));
+        image2.rotateBy(-20);
+        image2.setOrigin(0, image2.getHeight() / 2);
+        image2.setColor(Color.LIME);
+        Image image3 = new Image(new Texture("stripe/stripe_5.png"));
+        image3.setColor(Color.LIME);
+
+        Stack stack = new Stack();
+        stack.add(image);
+        stack.add(image2);
+        stack.add(image3);
+        table.add(stack).fillX().expandX();
+
+        Table verticalGroup = new Table();
+        verticalGroup.add(new PlanetActor(planetFactory.createPlanet())).pad(5);
+        verticalGroup.row();
+        verticalGroup.add(new PlanetActor(planetFactory.createPlanet())).pad(5);
+        verticalGroup.row();
+        verticalGroup.add(new PlanetActor(planetFactory.createPlanet()));
+        table.add(verticalGroup).pad(10);
         table.setFillParent(true);
         stage.addActor(table);
     }
