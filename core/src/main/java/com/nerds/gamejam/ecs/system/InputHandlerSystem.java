644d8a18242e8037f3c6824b58eae23be56b2241
JDKD <jdkd2110@gmail.com>
2020-11-15 14:12:03 +0000

    Limiting input based on InputUtil and turning on FPSLogging

--- core/src/main/java/com/nerds/gamejam/ecs/system/InputHandlerSystem.java
+++ core/src/main/java/com/nerds/gamejam/ecs/system/InputHandlerSystem.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.math.Shape2D;
 import com.badlogic.gdx.math.Vector3;
 import com.nerds.gamejam.ecs.component.BodyComponent;
 import com.nerds.gamejam.ecs.component.ClickableComponent;
+import com.nerds.gamejam.util.InputUtil;
 
 public class InputHandlerSystem extends BaseEntitySystem implements InputProcessor {
 
@@ -50,22 +51,24 @@ public class InputHandlerSystem extends BaseEntitySystem implements InputProcess
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        IntBag entityIds = getEntityIds();
+        if (InputUtil.INPUT_ALLOWED) {
+            IntBag entityIds = getEntityIds();
 
-        Vector3 unproject = orthographicCamera.unproject(new Vector3(screenX, screenY, 0));
-        float worldX = unproject.x;
-        float worldY = unproject.y;
+            Vector3 unproject = orthographicCamera.unproject(new Vector3(screenX, screenY, 0));
+            float worldX = unproject.x;
+            float worldY = unproject.y;
 
-        for (int i = entityIds.size() - 1; i >= 0; i--) {
-            int entityId = entityIds.get(i);
-            BodyComponent bodyComponent = bodyComponentComponentMapper.get(entityId);
-            ClickableComponent clickableComponent = clickableComponentMapper.get(entityId);
+            for (int i = entityIds.size() - 1; i >= 0; i--) {
+                int entityId = entityIds.get(i);
+                BodyComponent bodyComponent = bodyComponentComponentMapper.get(entityId);
+                ClickableComponent clickableComponent = clickableComponentMapper.get(entityId);
 
-            Shape2D body = bodyComponent.physicalBody;
+                Shape2D body = bodyComponent.physicalBody;
 
-            if (body.contains(worldX, worldY)) {
-                if (clickableComponent.clickable.onClick(worldX, worldY, screenX, screenY, button)) {
-                    return true;
+                if (body.contains(worldX, worldY)) {
+                    if (clickableComponent.clickable.onClick(worldX, worldY, screenX, screenY, button)) {
+                        return true;
+                    }
                 }
             }
         }
